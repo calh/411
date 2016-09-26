b8a053d2f3465572601fd99f6d7321c505345759
Cal Heldenbrand <cal@fbsdata.com>
2016-09-26 13:06:42 -0500

    Special docker config for use with --link

---
+++ docker/config.php
@@ -0,0 +1,80 @@
+<?php
+
+$config = [];
+
+/**
+ *
+ * Database configuration
+ *
+ */
+$config['db'] = [
+    'dsn' => 'sqlite:' . realpath("/data/data.db"),
+    'user' => 'root',
+    'pass' => null,
+];
+
+
+/**
+ *
+ * Search type configuration
+ * Note: All hostnames should specify protocol!
+ *
+ */
+
+# Elasticsearch configuration
+$config['elasticsearch'] = [
+    # Configuration for the 411 Alerts index.
+    'alerts' => [
+        'hosts' => ['http://eshost:9200'],
+        'index_hosts' => [],
+        'ssl_cert' => null,
+        'index' => null,
+        'date_based' => false,
+        'date_field' => 'alert_date',
+        'src_url' => null,
+    ],
+    # Configuration for the logstash index that 411 queries.
+    'logstash' => [
+        'hosts' => ['http://eshost:9200'],
+        'index_hosts' => [],
+        'ssl_cert' => null,
+        'index' => 'logstash',
+        'date_based' => true,
+        'date_field' => '@timestamp',
+        'src_url' => null,
+    ],
+];
+
+# Graphite
+# Fill in to enable the Graphite search
+$config['graphite'] = [
+    'host' => null,
+];
+
+# Threatexchange
+# Fill in to enable the Threatexchange search
+$config['threatexchange'] = [
+    'api_token' => null,
+    'api_secret' => null,
+];
+
+
+/**
+ *
+ * Target configuration
+ *
+ */
+
+# Jira integration
+# Fill in to allow 411 to generate Jira tickets.
+$config['jira'] = [
+    'host' => null,
+    'user' => null,
+    'pass' => null,
+];
+
+# Slack integration
+# Fill in to allow 411 to send messages to Slack.
+$config['slack'] = [
+    'webhook_url' => null
+];
